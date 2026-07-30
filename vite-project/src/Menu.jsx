8b3c7e1323491f60518af64c90bfd7c825193ebe
kent <kent.26pg@gmail.com>
2026-07-30 11:14:50 +0200

    menu component to render list of dishes

--- vite-project/src/Menu.jsx
+++ vite-project/src/Menu.jsx
@@ -1,1 +1,13 @@
+import Dish from "./Dish";
+
+function Menu({ dishes }) {
+  return (
+    <div className="Menu">
+      {dishes.map((dish) => (
+        <Dish key={dish.id} dish={dish} />
+      ))}
+    </div>
+  );
+}
+
 export default Menu;
